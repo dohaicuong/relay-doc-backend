a592884a4272f9023c4e335fc2aac8637fcb6e06
Eric Do <beatyshot@gmail.com>
2022-11-13 00:26:17 +1000

    clean up

--- api/graphql.ts
+++ api/graphql.ts
@@ -1,12 +1,15 @@
 import { createYoga } from 'graphql-yoga'
 import { schema } from '../server/schema'
-// import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 export const config = {
   api: { bodyParser: false }
 }
 
-export default createYoga({
+export default createYoga<{
+  req: NextApiRequest
+  res: NextApiResponse
+}>({
   graphqlEndpoint: '/api/graphql',
   schema,
 })
--- server/index.ts
+++
@@ -1,6 +0,0 @@
-import { createServer } from '@graphql-yoga/node'
-import { schema } from './schema'
-
-export const server = createServer({
-  schema,
-})
