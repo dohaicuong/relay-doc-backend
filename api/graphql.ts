d87ae57af7fa0619c07acdebf505ca2dd9d0b18f
Eric Do <beatyshot@gmail.com>
2022-11-13 00:34:04 +1000

    remove nextjs dep

--- api/graphql.ts
+++ api/graphql.ts
@@ -1,15 +1,11 @@
 import { createYoga } from 'graphql-yoga'
 import { schema } from '../server/schema'
-import type { NextApiRequest, NextApiResponse } from 'next'
 
 export const config = {
   api: { bodyParser: false }
 }
 
-export default createYoga<{
-  req: NextApiRequest
-  res: NextApiResponse
-}>({
+export default createYoga({
   graphqlEndpoint: '/api/graphql',
   schema,
 })
